0453ded2accb6e32d3b3c35407d03a0402e6ea3f
raksahb <bhaskar.athma@gmail.com>
2025-05-31 23:45:57 -0700

    Updated line tracking data handling to include y-coordinates for head and tail

--- Pybricks/line_follower.py
+++ Pybricks/line_follower.py
@@ -4,14 +4,16 @@ from pybricks.robotics import DriveBase
 from pybricks.pupdevices import Motor
 
 pr = PUPRemoteHub(Port.F)
-pr.add_channel('line','hhb') # Pass two 'h'alf ints: x coordinate of line head, and of line tail.
+pr.add_channel('line','hhhhb') # Pass two 'h'alf ints: x coordinate of line head, and of line tail.
 
 lm = Motor(Port.B, Direction.COUNTERCLOCKWISE)
 rm = Motor(Port.A)
 db = DriveBase(lm, rm, 56, 60+28) # 56 mm wheel diameter, 60 mm wheel base + 28 mm offset for the line sensor.
 
 while 1:
-    x_head, x_tail, line_seen = pr.call('line')
+    x_head, y_head, x_tail, y_tail, line_seen = pr.call('line')
+    print(f"line_seen - {line_seen} Head: ({x_head}, {y_head}) Tail: ({x_tail}, {y_tail})")
+
     steer = (x_head * 3 + x_tail * 10) / 13
     if line_seen:
         speed = 70
