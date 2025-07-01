ad0a1e529d2652b60448acac0223361a925bed3a
raksahb <bhaskar.athma@gmail.com>
2025-07-01 11:48:14 -0700

    MAIN: --wip--

--- Pybricks/line_follower.py
+++ Pybricks/line_follower.py
@@ -8,15 +8,17 @@ pr.add_channel('line', 'hhhhb') # Pass two 'h'alf ints: x coordinate of line hea
 
 lm = Motor(Port.B, Direction.COUNTERCLOCKWISE)
 rm = Motor(Port.A)
-db = DriveBase(lm, rm, 56, 60+28) # 56 mm wheel diameter, 60 mm wheel base + 28 mm offset for the line sensor.
+db = DriveBase(lm, rm, wheel_diameter=56, axle_track=80)
 
 while 1:
     x_head, y_head, x_tail, y_tail, line_seen = pr.call('line')
     print(f"line_seen - {line_seen} Head: ({x_head}, {y_head}) Tail: ({x_tail}, {y_tail})")
 
-    steer = (x_head * 3 + x_tail * 10) / 13
+    steer = ((x_head - 160) * 3 + (x_tail - 160) * 10) / 13
     if line_seen:
         speed = 70
     else:
         speed = 0
-    db.drive(speed, steer * 0.75)
+
+    # speed mm/s and turn_rate = deg/s
+    db.drive(speed, turn_rate=steer * 0.75)
